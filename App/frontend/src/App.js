37a879bf6170755a5393f695adb281cefcc155f2
Paria Jafarian <003paria.j@gmail.com>
2025-02-25 13:59:00 -0500

    Replaced all the alert calls in Messages.js with toast.success/toast.info/toast.error

--- App/frontend/src/App.js
+++ App/frontend/src/App.js
@@ -5,7 +5,7 @@ import Messages from "./pages/Messages";
 import ForgotPassword from "./pages/ForgotPassword";
 
 //Toastify imports for notifications
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer, toast, Flip } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
@@ -17,7 +17,19 @@ function App() {
         <Route path="/Messages" element={<Messages />} />
         <Route path = "/ForgotPassword" element={<ForgotPassword />} />
       </Routes>
-      <ToastContainer />
+      <ToastContainer
+        position="top-center"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+        transition={Flip}
+      />
     </Router>
   );
 }
