c2514153527c639ac4479e59d9d89b26c299300e
Zatcheryy <zcohentravail@gmail.com>
2025-02-26 12:52:22 -0500

    test-commit-test

--- App/backend/tests/login.test.js
+++ App/backend/tests/login.test.js
@@ -35,8 +35,8 @@ describe('User API tests for Login page', () => {
         jest.resetAllMocks();
     });
 
-    const sample_username = "user_username";
-    const sample_password = "user_password";
+    const sample_username = "users_username";
+    const sample_password = "users_password";
 
     test('Create a new user given user credentials', async () => {
         const response = await request(app)
