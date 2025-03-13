8e5c68389190164d7a841c078614290c684d8e18
Zatcheryy <zcohentravail@gmail.com>
2025-03-13 16:45:11 -0400

    adjustments to logic

--- App/backend/tests/login.test.js
+++ App/backend/tests/login.test.js
@@ -35,8 +35,8 @@ describe('User API tests for Login page', () => {
         jest.resetAllMocks();
     });
 
-    const sample_username = str(Math.random() * 1000);
-    const sample_password = str(Math.random() * 1000);
+    const sample_username = Math.random() * 1000;
+    const sample_password = Math.random() * 1000;
 
     test('Create a new user given user credentials', async () => {
         const response = await request(app)
